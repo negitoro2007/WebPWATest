0451fa192137e72071eb2e4f0cdb92f51553a6ba
negitoro2007 <negitoro.2007sub@gmail.com>
2023-11-18 17:24:51 +0900

    キャッシュファイル変更

--- scrpits/sw.js
+++ scrpits/sw.js
@@ -1,7 +1,7 @@
 // Cache name
 const CACHE_NAME = "pwa-sample-caches-v1";
 // Cache targets
-const urlsToCache = ["./", "./index.html"];
+const urlsToCache = ["./"];
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
