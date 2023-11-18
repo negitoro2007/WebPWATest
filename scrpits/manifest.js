09b14b0406f0fafdcac92605c6f64cfd86553e87
negitoro2007 <negitoro.2007sub@gmail.com>
2023-11-18 17:15:22 +0900

    flja

--- scrpits/manifest.js
+++ scrpits/manifest.js
@@ -1,6 +1,9 @@
 function set(name, img) {
     var myUrl = window.location.href;
-
+    let url_string = window.location.href;
+    let url = new URL(url_string);
+    let data = url.searchParams.get("url");
+    console.log(data);
     var manifest = {
         name: "PWA Test",
         short_name: "PWA",
@@ -18,9 +21,11 @@ function set(name, img) {
     };
     manifest.name = name;
     manifest.icons[0].src = img;
+    manifest.start_url = data;
     const stringManifest = JSON.stringify(manifest);
     const blob = new Blob([stringManifest], { type: "application/json" });
     const manifestURL = URL.createObjectURL(blob);
     document.querySelector("#my-manifest").setAttribute("href", manifestURL);
 }
-//set("test", "images/web2app4.png");
+
+set("test", "images/web2app4.png");
